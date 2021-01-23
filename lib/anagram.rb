3f5c3d4d5bbbde73dfa68044c08429db3aabe75d
SuperPox <calebkohl@gmail.com>
2021-01-23 16:56:29 -0700

    commit tree e7c49c2fa12bbe9611043f7943f639e54a91f521

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word  
+    end
+
+    def match(array) #["hello", "world", "zombies", "pants", "dipper"]
+        # word = "diaper"
+        # grab each word in the array, sort it alphabetically, compare it to @word, if match then anagram
+        temp = []
+        testArray = []
+        wordArray = []
+        wordArray << @word
+
+        array.each do |i|
+            if i.chars.sort.join == wordArray.join.chars.sort.join  #wordArray = "adeipr"
+                temp << i
+            end
+        end
+        return temp
+    end
+end
+
+#'allergy'
+#(gallery ballerina regally clergy largely leading)
